378f2fcddb5ad01ef859b190d2b978a9eff165a8
indoctrinatedrecluse <AbMitra1999@gmail.com>
2021-08-16 19:33:28 +0530

    Slightly more elegant

--- Test1_Primes/com/primefactor/logic/PrimeFactorLogic.java
+++ Test1_Primes/com/primefactor/logic/PrimeFactorLogic.java
@@ -1,11 +1,12 @@
 package com.primefactor.logic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class PrimeFactorLogic {
 	
 	public List<Integer> getFactor(int number) {
-		return Arrays.asList();
+		return new ArrayList<>();
 	}
 }
