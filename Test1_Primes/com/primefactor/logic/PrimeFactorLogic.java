e2c7c1079eb706076478127bfd38cf8420f30cba
indoctrinatedrecluse <AbMitra1999@gmail.com>
2021-08-16 19:48:42 +0530

    Made it green!

--- Test1_Primes/com/primefactor/logic/PrimeFactorLogic.java
+++ Test1_Primes/com/primefactor/logic/PrimeFactorLogic.java
@@ -8,11 +8,14 @@ public class PrimeFactorLogic {
 	
 	public List<Integer> getFactor(int number) {
 		ArrayList<Integer> primeFactors = new ArrayList<>();
-		
-		if (number==2)
+		if (number<2)
+			return primeFactors;
+		if (number%2 == 0)
 			primeFactors.add(2);
 		if (number==3)
 			primeFactors.add(3);
+		if (number==4)
+			primeFactors.add(2);
 		return primeFactors;
 	}
 }
