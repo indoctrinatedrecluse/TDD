4c86b41e4b924224c78d3abe9b8fefdf6905980c
indoctrinatedrecluse <AbMitra1999@gmail.com>
2021-08-16 19:32:17 +0530

    Green for Factors of Zero

--- Test1_Primes/com/primefactor/logic/PrimeFactorLogic.java
+++ Test1_Primes/com/primefactor/logic/PrimeFactorLogic.java
@@ -1,8 +1,11 @@
 package com.primefactor.logic;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class PrimeFactorLogic {
 	
-	public void getFactor() {
-		return;
+	public List<Integer> getFactor(int number) {
+		return Arrays.asList();
 	}
 }
